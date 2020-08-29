68f9f04ff80a199055014a1b991674f90fdd905a
Ilatyphi95 <ilatyphi95@gmail.com>
2020-08-29 13:38:41 +0100

    Design start page

--- app/src/main/java/com/ilatyphi95/gads2020leaderboard/MainActivity.kt
+++ app/src/main/java/com/ilatyphi95/gads2020leaderboard/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.ilatyphi95.gads2020leaderboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val counter = MutableLiveData<Int>()
+
+    private val job = CoroutineScope(Job()  + Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvSkip = findViewById<TextView>(R.id.tv_skip)
+        tvSkip.setOnClickListener{
+            navigateToLeaderBoard()
+        }
+
+        counter.observe(this, { count ->
+            tvSkip.text = getString(R.string.skip_in_second, count)
+        })
+
+        job.launch { startCountDown(counter) }
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
+
+    private fun navigateToLeaderBoard() {
+        startActivity(Intent(this, LeaderBoardActivity::class.java))
+    }
+
+    private suspend fun startCountDown(counter: MutableLiveData<Int>) {
+        for (count in 3 downTo 0) {
+            counter.postValue(count)
+            delay(1000)
+        }
+
+        navigateToLeaderBoard()
     }
 }
