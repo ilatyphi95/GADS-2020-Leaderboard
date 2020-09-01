df30bfba2bd544eb396beb37858b822045a92127
Ilatyphi95 <ilatyphi95@gmail.com>
2020-09-01 11:12:07 +0100

    Create API endpoint data classes

---
+++ app/src/main/java/com/ilatyphi95/gads2020leaderboard/IQLeader.kt
@@ -0,0 +1,8 @@
+package com.ilatyphi95.gads2020leaderboard
+
+data class IQLeader(
+    val name: String,
+    val score: Int,
+    val country: String,
+    val badgeUrl: String
+)
---
+++ app/src/main/java/com/ilatyphi95/gads2020leaderboard/LearningLeader.kt
@@ -0,0 +1,8 @@
+package com.ilatyphi95.gads2020leaderboard
+
+data class LearningLeader(
+    val name: String,
+    val hours: Int,
+    val country: String,
+    val badgeUrl: String
+)
