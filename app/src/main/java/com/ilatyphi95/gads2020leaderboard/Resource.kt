003e14d54bad6c821325f02bcfb7b6e5c04556c2
Ilatyphi95 <ilatyphi95@gmail.com>
2020-09-01 14:45:56 +0100

    Add resource file

---
+++ app/src/main/java/com/ilatyphi95/gads2020leaderboard/Resource.kt
@@ -0,0 +1,23 @@
+package com.ilatyphi95.gads2020leaderboard
+
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+
+        fun <T> success(data: T):
+                Resource<T> = Resource(status = Status.SUCCESS, data = data, message = null)
+
+        fun <T> error(data: T?, message: String):
+                Resource<T> = Resource(status = Status.ERROR, data = data, message = message)
+
+        fun <T> loading(data: T):
+                Resource<T> = Resource(status = Status.LOADING, data = data, message = null)
+    }
+}
